8f65c2161955ea88dbd6784d9aca0b70a18f0892
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-22 18:39:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,7 @@ end
 def valid_move?(board, index)
   if position_taken?(board, index) == true
     false
-  elsif index > 9 || index < 0
+  elsif index > 8 || index < 0
     false
   else
     true
